068e515371c1b18a65f03f64031d27c054b5106d
laktam <laktamlaktamabdellah@gmail.com>
2024-09-14 23:28:43 +0100

    init project

---
+++ src/main/java/org/mql/laktam/speedreadbackend/SpeedReadBackendApplication.java
@@ -0,0 +1,13 @@
+package org.mql.laktam.speedreadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedReadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedReadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mql/laktam/speedreadbackend/SpeedReadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mql.laktam.speedreadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedReadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
