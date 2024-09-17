e47d4cd46dd070d711cf863cf3468bd6c37d9891
laktam <laktamlaktamabdellah@gmail.com>
2024-09-17 12:12:39 +0100

    add LikeRepository

---
+++ src/main/java/org/mql/laktam/speedreadbackend/repositories/LikeRepository.java
@@ -0,0 +1,8 @@
+package org.mql.laktam.speedreadbackend.repositories;
+
+import org.mql.laktam.speedreadbackend.models.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+}
