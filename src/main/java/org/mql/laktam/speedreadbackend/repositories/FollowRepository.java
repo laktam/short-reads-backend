94dd0bacee66cbb6a44523cd3b18f4afaeefa01d
laktam <laktamlaktamabdellah@gmail.com>
2024-09-17 12:13:37 +0100

    add FollowRepository

---
+++ src/main/java/org/mql/laktam/speedreadbackend/repositories/FollowRepository.java
@@ -0,0 +1,8 @@
+package org.mql.laktam.speedreadbackend.repositories;
+
+import org.mql.laktam.speedreadbackend.models.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+}
