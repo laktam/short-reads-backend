a6727dd764c69bd54cee012b7e46d5db9f3e0674
laktam <laktamlaktamabdellah@gmail.com>
2024-09-17 12:14:27 +0100

    add TagRepository

---
+++ src/main/java/org/mql/laktam/speedreadbackend/repositories/TagRepository.java
@@ -0,0 +1,8 @@
+package org.mql.laktam.speedreadbackend.repositories;
+
+import org.mql.laktam.speedreadbackend.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+}
