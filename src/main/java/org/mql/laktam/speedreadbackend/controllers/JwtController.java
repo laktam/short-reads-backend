dadf4c965137d5ca7ceafeac7024d842140d6f34
laktam <laktamlaktamabdellah@gmail.com>
2024-09-21 00:17:05 +0100

    update login endpoint to return error message or jwt token

--- src/main/java/org/mql/laktam/speedreadbackend/controllers/JwtController.java
+++ src/main/java/org/mql/laktam/speedreadbackend/controllers/JwtController.java
@@ -10,6 +10,7 @@ import org.mql.laktam.speedreadbackend.models.jwt.JwtLoginRequest;
 import org.mql.laktam.speedreadbackend.models.jwt.JwtResponse;
 import org.mql.laktam.speedreadbackend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -42,15 +43,16 @@ public class JwtController {
 	// Get a JWT Token once user is authenticated, otherwise throw
 	// BadCredentialsException
 	@PostMapping("/login")
-	public ResponseEntity<JwtResponse> createToken(@RequestBody JwtLoginRequest request) throws Exception {
+	public ResponseEntity<?> createToken(@RequestBody JwtLoginRequest request) throws Exception {
 		try {
 			authenticationManager.authenticate(
 					new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
 		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User is disabled");
 		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
 		}
+		
 		final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
 		final String jwtToken = tokenManager.generateJwtToken(userDetails);
 		return ResponseEntity.ok(new JwtResponse(jwtToken));
