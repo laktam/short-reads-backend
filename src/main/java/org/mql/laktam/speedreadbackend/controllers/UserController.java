a211bba7b3249a06346c5f14a2fef228d15c2c97
laktam <laktamlaktamabdellah@gmail.com>
2024-09-22 12:19:05 +0100

    add UserController and get user by username method

---
+++ src/main/java/org/mql/laktam/speedreadbackend/controllers/UserController.java
@@ -0,0 +1,47 @@
+package org.mql.laktam.speedreadbackend.controllers;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import org.mql.laktam.speedreadbackend.jwtutils.JwtUserDetailsService;
+import org.mql.laktam.speedreadbackend.jwtutils.TokenManager;
+import org.mql.laktam.speedreadbackend.models.User;
+import org.mql.laktam.speedreadbackend.models.jwt.JwtSignupRequest;
+import org.mql.laktam.speedreadbackend.models.jwt.JwtLoginRequest;
+import org.mql.laktam.speedreadbackend.models.jwt.JwtResponse;
+import org.mql.laktam.speedreadbackend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/users")
+public class UserController {
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostMapping("/username/{username}")
+	public ResponseEntity<?> getUserByUsername(@PathVariable String username){
+		Optional<User> userOpt = userRepository.findByUsername(username);
+		if(userOpt.isPresent()) {
+			return ResponseEntity.ok(userOpt.get());
+		}else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("User not found");
+		}
+	}
+
+}
