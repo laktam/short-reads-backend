ef2359f2a8f99ca00bd5fcf9786f15184141b504
laktam <laktamlaktamabdellah@gmail.com>
2024-09-25 14:24:45 +0100

    add follow repository and isFollowing method

--- src/main/java/org/mql/laktam/speedreadbackend/repositories/FollowRepository.java
+++ src/main/java/org/mql/laktam/speedreadbackend/repositories/FollowRepository.java
@@ -2,7 +2,13 @@ package org.mql.laktam.speedreadbackend.repositories;
 
 import org.mql.laktam.speedreadbackend.models.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+	@Query("SELECT CASE WHEN COUNT(f) > 0 THEN true ELSE false END FROM Follow f " +
+	           "WHERE f.follower.username = :followerUsername AND f.followed.username = :followedUsername")
+	boolean isFollowing(@Param("followerUsername") String followerUsername, 
+	                                        @Param("followedUsername") String followedUsername);
 
 }
