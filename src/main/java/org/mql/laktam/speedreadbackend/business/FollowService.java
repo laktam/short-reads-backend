cb3066dd03b68ba63726d431021d1a66965b181b
laktam <laktamlaktamabdellah@gmail.com>
2024-09-26 14:22:56 +0100

    add follow and unfollow methods to follow repository

--- src/main/java/org/mql/laktam/speedreadbackend/business/FollowService.java
+++ src/main/java/org/mql/laktam/speedreadbackend/business/FollowService.java
@@ -2,4 +2,7 @@ package org.mql.laktam.speedreadbackend.business;
 
 public interface FollowService {
 	public boolean isFollowing(String followerUsername, String followedUsername);
+	public void followUser(String followerUsername, String followedUsername);
+    public void unfollowUser(String followerUsername, String followedUsername);
+
 }
--- src/main/java/org/mql/laktam/speedreadbackend/business/FollowServiceDefault.java
+++ src/main/java/org/mql/laktam/speedreadbackend/business/FollowServiceDefault.java
@@ -1,17 +1,44 @@
 package org.mql.laktam.speedreadbackend.business;
 
+import org.mql.laktam.speedreadbackend.models.Follow;
+import org.mql.laktam.speedreadbackend.models.User;
 import org.mql.laktam.speedreadbackend.repositories.FollowRepository;
+import org.mql.laktam.speedreadbackend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FollowServiceDefault implements FollowService{
 	@Autowired
 	private FollowRepository followRepository;
-
+	@Autowired
+    private UserRepository userRepository;
+	
 	@Override
 	public boolean isFollowing(String followerUsername, String followedUsername) {
 		return followRepository.isFollowing(followerUsername, followedUsername);
 	}
 
+	
+	public void followUser(String followerUsername, String followedUsername) {
+        User follower = userRepository.findByUsername(followerUsername)
+            .orElseThrow(() -> new UsernameNotFoundException("Follower not found"));
+        User followed = userRepository.findByUsername(followedUsername)
+            .orElseThrow(() -> new UsernameNotFoundException("User to follow not found"));
+        Follow follow = new Follow(follower, followed);
+        followRepository.save(follow);
+    }
+
+    public void unfollowUser(String followerUsername, String followedUsername) {
+        User follower = userRepository.findByUsername(followerUsername)
+            .orElseThrow(() -> new UsernameNotFoundException("Follower not found"));
+        User followed = userRepository.findByUsername(followedUsername)
+            .orElseThrow(() -> new UsernameNotFoundException("User to unfollow not found"));
+
+        Follow follow = followRepository.findByFollowerAndFollowed(follower, followed)
+            .orElseThrow(() -> new IllegalStateException("Follow relationship does not exist"));
+
+        followRepository.delete(follow);
+    }
 }
