712f409492e9cab9fa34dab5a3b58430228a0a68
laktam <laktamlaktamabdellah@gmail.com>
2024-10-08 22:15:23 +0100

    add findAllByUsername and findPostsByUsername for pagination

--- src/main/java/org/mql/laktam/speedreadbackend/repositories/PostRepository.java
+++ src/main/java/org/mql/laktam/speedreadbackend/repositories/PostRepository.java
@@ -1,8 +1,17 @@
 package org.mql.laktam.speedreadbackend.repositories;
 
+import java.util.List;
+
 import org.mql.laktam.speedreadbackend.models.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+	@Query("SELECT p FROM Post p WHERE p.user.username = ?1")
+    List<Post> findAllByUsername(String username);
 
+	@Query("SELECT p FROM Post p WHERE p.user.username = ?1")
+	Page<Post> findPostsByUsername(String username, Pageable pageable);// for pagination to not load all posts at once
 }
